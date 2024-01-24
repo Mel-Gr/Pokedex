1f471b4020585ee6f095a77380a098583da80ac9
diegoher125 <diegoher125.clases@gmail.com>
2024-01-24 18:40:12 +0100

    Add pokemon info to an object

--- script/main.js
+++ script/main.js
@@ -16,61 +16,54 @@ boton.addEventListener("click", () => {
         return response.json();
     }).then((data) => {
 
-        let especies = document.createElement("div");
-        let habitat = document.createElement("div");
-        let texto = document.createElement("div");
-        let nombre = document.createElement("div");
-        let altura = document.createElement("div");
-        let peso = document.createElement("div");
-        let numPokemon = document.createElement("div");
-        let tipo = document.createElement("div");
-
-        if (data.sprites.front_default !== null)
-            addImage(data.sprites.front_default);
-        if (data.sprites.front_female !== null)
-            addImage(data.sprites.front_female);
-        if (data.sprites.front_shiny !== null)
-            addImage(data.sprites.front_shiny);
-        if (data.sprites.front_shiny_female !== null)
-            addImage(data.sprites.front_shiny_female);
-        if (data.sprites.back_default !== null)
-            addImage(data.sprites.back_default);
-        if (data.sprites.back_female !== null)
-            addImage(data.sprites.back_female);
-        if (data.sprites.back_shiny !== null)
-            addImage(data.sprites.back_shiny);
-        if (data.sprites.back_shiny_female !== null)
-            addImage(data.sprites.back_shiny_female);
-
-        numPokemon.innerHTML = "Num. Pokedex: " + ((data.hasOwnProperty('id')) ? JSON.stringify(data.id) : "");
-        tipo.innerHTML = "Type: " + ((data.types.hasOwnProperty('0')) ? JSON.stringify(data.types[0].type.name) : "")
-            + ((data.types.hasOwnProperty('1')) ? "/" + JSON.stringify(data.types[1].type.name) : "");
-        nombre.innerHTML = "Name: " + ((data.forms.hasOwnProperty('0')) ? JSON.stringify(data.forms[0].name) : "");
-        altura.innerHTML = "Height: " + ((data.hasOwnProperty('height')) ? JSON.stringify(data.height) : "" + "cm");
-        peso.innerHTML = "Weight: " + ((data.hasOwnProperty('weight')) ? JSON.stringify(data.weight) : "" + "Kg");
+        let pokemon = {
+            nombre: ((data.forms.hasOwnProperty('0')) ? JSON.stringify(data.forms[0].name) : ""),
+            altura: ((data.hasOwnProperty('height')) ? JSON.stringify(data.height) : ""),
+            peso: ((data.hasOwnProperty('weight')) ? JSON.stringify(data.weight) : ""),
+            numPokemon: ((data.hasOwnProperty('id')) ? JSON.stringify(data.id) : ""),
+            tipo1: ((data.types.hasOwnProperty('0')) ? JSON.stringify(data.types[0].type.name) : ""),
+            tipo2: ((data.types.hasOwnProperty('1')) ? "/" + JSON.stringify(data.types[1].type.name) : ""),
+            front_default: ((data.sprites.front_default !== null) ? data.sprites.front_default : ""),
+            front_female: ((data.sprites.front_female !== null) ? data.sprites.front_female : ""),
+            front_shiny: ((data.sprites.front_shiny !== null) ? data.sprites.front_shiny : ""),
+            front_shiny_female: ((data.sprites.front_shiny_female !== null) ? data.sprites.front_shiny_female : ""),
+            back_default: ((data.sprites.back_default !== null) ? data.sprites.back_default : ""),
+            back_female: ((data.sprites.back_female !== null) ? data.sprites.back_female : ""),
+            back_shiny: ((data.sprites.back_shiny !== null) ? data.sprites.back_shiny : ""),
+            back_shiny_female: ((data.sprites.back_shiny_female !== null) ? data.sprites.back_shiny_female : ""),
+            especie,
+            habitat,
+            texto
+        };
+        let info = document.createElement("div");
+        info.innerHTML += "Num. Pokedex: " + pokemon.numPokemon;
+        info.innerHTML += "Type: " + pokemon.tipo1 + pokemon.tipo2;
+        info.innerHTML += "Name: " + pokemon.nombre;
+        info.innerHTML += "Height: " + pokemon.peso;
+        info.innerHTML += "Weight: " + pokemon.altura;
 
         let url2 = data.species.url;
         fetch(url2).then((respuesta) => {
             return respuesta.json();
         }).then((datos) => {
-            especies.innerHTML = "Species: " +
-                ((datos.genera[5].hasOwnProperty('genus')) ? JSON.stringify(datos.genera[5].genus) : "");
-            habitat.innerHTML = "Habitat: " +
-                ((datos.hasOwnProperty('habitat')) ? JSON.stringify(datos.habitat.name) : "");
-            texto.innerHTML = "Description: " +
-                ((datos.flavor_text_entries[34].hasOwnProperty('flavor_text')) ? JSON.stringify(datos.flavor_text_entries[34].flavor_text) : "");
+            pokemon.especie = ((datos.genera.hasOwnProperty('5')) ? JSON.stringify(datos.genera[5].genus) : "");
+            pokemon.habitat = ((datos.hasOwnProperty('habitat')) ? JSON.stringify(datos.habitat.name) : "");
+            pokemon.texto = ((datos.flavor_text_entries.hasOwnProperty('34')) ? JSON.stringify(datos.flavor_text_entries[34].flavor_text) : "");
+            info.innerHTML += "Species: " + pokemon.especie;
+            info.innerHTML += "Habitat: " + pokemon.habitat;
+            info.innerHTML += "Description: " + pokemon.texto;
         }).catch((error2) => {
             console.error("Error en la consulta: " + error2);
         })
-
-        div.appendChild(tipo);
-        div.appendChild(altura);
-        div.appendChild(peso);
-        div.appendChild(numPokemon);
-        div.appendChild(nombre);
-        div.appendChild(especies);
-        div.appendChild(habitat);
-        div.appendChild(texto);
+        addImage(pokemon.front_default);
+        addImage(pokemon.front_female);
+        addImage(pokemon.front_shiny);
+        addImage(pokemon.front_shiny_female);
+        addImage(pokemon.back_default);
+        addImage(pokemon.back_female);
+        addImage(pokemon.back_shiny);
+        addImage(pokemon.back_shiny_female);
+        div.appendChild(info);
     }).catch((error) => {
         console.error("Error en la consulta: " + error);
     })
